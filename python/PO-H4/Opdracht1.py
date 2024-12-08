2c03e9b6ef5f2bbd054f104fbb50da8c5da688aa
Odinsson25 <64733407+Odinsson25@users.noreply.github.com>
2024-12-08 17:58:31 +0100

    Comments met uitleg toegevoegd

--- python/PO-H4/Opdracht1.py
+++ python/PO-H4/Opdracht1.py
@@ -1,20 +1,29 @@
-import math
+import math # Importeer math
 
-oppervlakte = float(input("Wat is de oppervlakte van de kamer in m²?"))
+print("LET OP: Voer decimalen in als \"5.2\", niet als \"5,2\"") # Om fouten te voorkomen
 
-if not oppervlakte:
+oppervlakte = float(input("Wat is de oppervlakte van de kamer in m²?") ) # Vraag om de opervlakkte van de kamer
+
+
+# Een andere manier waarop je de 2 regel hieboven kan toepassen, is op deze manier:
+# oppervlakte = float(input("Wat is de oppervlakte van de kamer in m²?").replace(",", ".")) # Vraag om de opervlakkte van de kamer
+#                                                                     Vervang de , met een .
+
+
+if not oppervlakte: # Als er GEEN oppervlakte is
     print("geen geldige input!")
 
 print("De kamer is "+ str(oppervlakte) +" m²")
 
-oppervlakte *= 1.1
-oppervlakte = round(oppervlakte,2)
+oppervlakte *= 1.1 # Doe de oppervlakte keer 1.1   OF  oppervlakte = oppervlakte * 1.1
+
+oppervlakte = round(oppervlakte, 2) # Rond de oppervlakte af naar 2 decimalen 
 print("Er is "+ str(oppervlakte) +" m² nodig.")
 
-pakken = math.ceil(oppervlakte/2.47)
+pakken = math.ceil(oppervlakte/2.47) # Bereken het aantal pakken. Je kan geen halve pakken hebben, dus rond je af naar het eerstvolgende pak
 print("Je hebt "+str(pakken)+" pakken nodig.")
 
-prijs = pakken * 29.62
+prijs = pakken * 29.62 # Bereken de prijs
 print("Het kost je €" +str(prijs))
 
 print("\nGemaakt door [NAAM]")
--- python/PO-H4/Opdracht2.py
+++ python/PO-H4/Opdracht2.py
@@ -1,22 +1,24 @@
-print("LET OP: Voer decimalen in als \"5.2\", niet als \"5,2\"")
+print("LET OP: Voer decimalen in als \"5.2\", niet als \"5,2\"") # Om fouten te voorkomen
 
-def gemiddelde(cijfer1, weging1, cijfer2, weging2):
-    print("Cijfer 1: " + str(cijfer1)+", weging "+str(weging1))
+def gemiddelde(cijfer1, weging1, cijfer2, weging2): # Maak de functie aan
+    print("Cijfer 1: " + str(cijfer1)+", weging "+str(weging1)) # laat cijfer 1 en 2 nogmaals zien, met bijbehordende weging 
     print("Cijfer 2: " + str(cijfer2)+", weging "+str(weging2))
-    gemiddeld = round(((cijfer1 * weging1 + cijfer2* weging2)/(weging1+weging2)), 1)
-    print("Jouw gemiddelde is een " + str(gemiddeld))
-    totaleWeging = weging1 + weging2
-    print("\n")
-    return gemiddeld
+
+    gemiddeld = round(((cijfer1 * weging1 + cijfer2* weging2)/(weging1+weging2)), 1) # Afronden naar 1 decimaal 
+
+    print("Jouw gemiddelde is een " + str(gemiddeld)) # Stuur het gemiddelde terug naar de gebruiker
+    print("\n") # Maak een lege lijn
+    return gemiddeld # Stuur het gemiddelde terug en beëindig de functie
 
     
+# Deze variabelen hebben 'Inp' in hun naam, hiermee weet je dat het om de input gaat.\
+cijfer1Inp = float(input("Voer cijfer 1 in:\t")) # Vraag om het cijfer
+weging1Inp = float(input("Voer de weging voor cijfer 1 in:\t")) or 1 # Vraag om de weging, geen weging => weging = 1
 
-cijfer1Inp = float(input("Voer cijfer 1 in:\t")) 
-weging1Inp = float(input("Voer de weging voor cijfer 1 in:\t")) 
+cijfer2Inp= float(input("Voer cijfer 2 in:\t")) # Vraag om het cijfer
+weging2Inp = float(input("Voer de weging voor cijfer 2 in:\t")) or 1 # Vraag om de weging, geen weging => weging = 1
 
-cijfer2Inp= float(input("Voer cijfer 2 in:\t")) or 8.9
-weging2Inp = float(input("Voer de weging voor cijfer 2 in:\t")) 
+gemiddelde(cijfer1Inp, weging1Inp,cijfer2Inp, weging2Inp) # Roep de functies aan met bijbehorende argumenten
 
-gemiddelde(cijfer1Inp, weging1Inp,cijfer2Inp, weging2Inp,)
 
 print("\nVeel succes met je toetsen!\n\nGemaakt door [NAAM]")
